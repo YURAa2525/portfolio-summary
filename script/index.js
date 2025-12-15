14af5da6bcad41e6b41ef028b90789380d2ce9b3
YURAa2525 <yut2094vb@ezweb.ne.jp>
2025-12-16 02:30:11 +0900

    a

--- script/index.js
+++ script/index.js
@@ -4,7 +4,16 @@ $(function() {
 
 
 function filterBtn() {
+  let isWait = false;
+
   $(".header").on("click mouseenter", ".js-filter-btn", function() {
+    if (isWait) return;
+    isWait = true;
+    
     $(".js-filter-screen").toggleClass("is-show");
+
+    setTimeout(() => {
+      isWait = false;
+    }, 200);
   });
 }
