28567b5e8e6d1b55c3994a9f34b104ddc54aae4b
YURAa2525 <yut2094vb@ezweb.ne.jp>
2025-12-16 17:59:30 +0900

    a

--- script/index.js
+++ script/index.js
@@ -1,5 +1,6 @@
 $(function() {
   filterBtn();
+  slidePage();
 });
 
 
@@ -9,8 +10,6 @@ function filterBtn() {
   $(".header").on("click pointerenter", ".js-filter-btn", function(e) {
     if ((e.type == "pointerenter") && (e.pointerType != "mouse")) return;
 
-    console.log(e.pointerType);
-
     if (isWait) return;
     isWait = true;
     
@@ -19,8 +18,34 @@ function filterBtn() {
     setTimeout(() => {
       isWait = false;
     }, 200);
-
-
-    $(".debug-console").text(e.type);
   });
 }
+
+
+function slidePage() {
+  const maxPageIx  = $(".js-slide-page").length - 1;
+  let   showPageIx = 0;
+
+  $(".main-screen").on("wheel", function(e) {
+    if (e.originalEvent.deltaY != 0) return;
+
+    if (0 < e.originalEvent.deltaX) {
+      showPageIx = Math.min(showPageIx + 1, maxPageIx);
+    }
+    else {
+      showPageIx = Math.max(showPageIx - 1, 0);
+    }
+
+    const moveX = $(".js-slide-page").width() * showPageIx * -1;
+    $(".js-pages").css("transform", `translateX(${moveX}px)`);
+  });
+}
+
+
+
+// ポインタ特性で判定
+// if (window.matchMedia("(pointer: coarse)").matches) {
+//   console.log("タッチデバイス（スマホ・タブレット）");
+// } else if (window.matchMedia("(pointer: fine)").matches) {
+//   console.log("マウス操作が可能なデバイス（PC）");
+// }
